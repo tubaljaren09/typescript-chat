166b0113cd8d701f11d5698a48266e5e8c8e4688
Jaren <tubaljaren09@gmail.com>
2024-01-10 13:15:51 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "./Pages/Register";
+
+const App = (): React.JSX.Element => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default App;
