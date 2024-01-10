d2c27877f7beb393fc5a3e99bf076c84ff562dd1
Jaren <tubaljaren09@gmail.com>
2024-01-10 20:40:42 +0800

    create message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Message = (): React.JSX.Element => {
+  const [owner, setOwner] = useState<boolean>(true);
   return (
-    <div>
-      <h1>Message</h1>
-    </div>
+    <>
+      {owner ? (
+        <div className="flex flex-row-reverse gap-5 mb-5">
+          <div className="flex flex-col text-gray-500 font-light">
+            <img
+              className="w-10 h-10 rounded-[50%] object-cover"
+              src="https://images.pexels.com/photos/19745509/pexels-photo-19745509/free-photo-of-close-up-of-a-raccoon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+            />
+            <span>just now</span>
+          </div>
+          <div className="max-w-[80%] flex flex-col gap-2.5 items-end">
+            <p className="bg-[#8da4f1] text-white py-2.5 px-5 rounded-tl-xl rounded-br-xl rounded-bl-xl max-w-max">
+              hello
+            </p>
+            <img
+              className="w-[50%]"
+              src="https://images.pexels.com/photos/19745509/pexels-photo-19745509/free-photo-of-close-up-of-a-raccoon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="flex gap-5 mb-5">
+          <div className="flex flex-col text-gray-500 font-light">
+            <img
+              className="w-10 h-10 rounded-[50%] object-cover"
+              src="https://images.pexels.com/photos/19745509/pexels-photo-19745509/free-photo-of-close-up-of-a-raccoon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+            />
+            <span>just now</span>
+          </div>
+          <div className="max-w-[80%] flex flex-col gap-2.5">
+            <p className="bg-white py-2.5 px-5 rounded-tr-xl rounded-br-xl rounded-bl-xl max-w-max">
+              hello
+            </p>
+            <img
+              className="w-[50%]"
+              src="https://images.pexels.com/photos/19745509/pexels-photo-19745509/free-photo-of-close-up-of-a-raccoon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Message from "./Message";
 
-const Messages = () => {
+const Messages = (): React.JSX.Element => {
   return (
-    <div className="bg-[#ddddf7] p-2.5 h-[calc(100%-112px)]">
+    <div className="bg-[#ddddf7] p-2.5 h-[calc(100%-112px)] overflow-y-scroll">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
       <Message />
       <Message />
       <Message />
